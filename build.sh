6a8a1541da76d7a46dd38505d34269aee41a55d2
LsMacox <zugeraev2@gmail.com>
2021-12-01 15:00:49 +0300

    [added] build.sh

---
+++ build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -a
+
+FILE_ENV='.env'
+if [ ! -f "$FILE_ENV" ]; then
+    cp .env.example .env
+fi
+
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+if [ -n "$PHP_VERSION" ]; then PHP_VERSION='7.4'; fi
+if [ -n "$BUILD_GIT_BRANCH" ]; then BUILD_GIT_BRANCH='main'; fi
+if [ -z "$GITHUB_USERNAME" ]; then
+    echo 'the GITHUB_USERNAME variable was not found in the .env file'
+    exit 0
+fi
+if [ -z "$GITHUB_ACCESS_TOKEN" ]; then
+    echo 'the GITHUB_ACCESS_TOKEN variable was not found in the .env file'
+    exit 0
+fi
+
+COMPOSER_WORK_DIR=./
+COMPOSER_ARGS='-n --working-dir='"${COMPOSER_WORK_DIR}"' --ignore-platform-reqs'
+
+DIR_VENDOR='vendor'
+if [ -d "$DIR_VENDOR" ];
+then
+    composer update ${COMPOSER_ARGS}
+else
+    composer install --no-cache ${COMPOSER_ARGS}
+fi
+
+git fetch origin ${BUILD_GIT_BRANCH}
+git reset --hard origin/${BUILD_GIT_BRANCH}
+php artisan key:generate
+php artisan config:cache
+php artisan storage:link
