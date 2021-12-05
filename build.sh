71ca5c8bedd898c9e842ec63c897bab59d208aed
LsMacox <zugeraev2@gmail.com>
2021-12-05 14:57:24 +0300

    [modified] build

--- build.sh
+++ build.sh
@@ -7,26 +7,15 @@ if [ ! -f "$FILE_ENV" ]; then
     cp .env.example .env
 fi
 
-if [ -f .env ]; then
-    # Load Environment Variables
-    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
-fi
+export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 
-if [ -n "$PHP_VERSION" ]; then PHP_VERSION='7.4'; fi
-if [ -n "$BUILD_GIT_BRANCH" ]; then BUILD_GIT_BRANCH='main'; fi
-
-COMPOSER_ARGS='-n --ignore-platform-reqs'
+if [ -z "$PHP_VERSION" ]; then PHP_VERSION='7.4'; fi
+if [ -z "$BUILD_GIT_BRANCH" ]; then BUILD_GIT_BRANCH='main'; fi
 
 git fetch origin ${BUILD_GIT_BRANCH}
 git reset --hard origin/${BUILD_GIT_BRANCH}
 
-DIR_VENDOR='vendor'
-if [ -d "$DIR_VENDOR" ];
-then
-    composer update ${COMPOSER_ARGS}
-else
-    composer install --no-cache ${COMPOSER_ARGS}
-fi
+composer install
 
 php artisan key:generate
 php artisan migrate
