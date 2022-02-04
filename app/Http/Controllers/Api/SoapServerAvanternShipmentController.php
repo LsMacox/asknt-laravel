cc172f9c984a18a6a6a2768302871ee3ed897ee5
LsMacox <zugeraev2@gmail.com>
2022-02-04 12:07:14 +0300

    [fixed] func getWsdlUri in SoapServerAvanternShipmentController

--- app/Http/Controllers/Api/SoapServerAvanternShipmentController.php
+++ app/Http/Controllers/Api/SoapServerAvanternShipmentController.php
@@ -19,7 +19,7 @@ class SoapServerAvanternShipmentController extends AbstractSoapServerController
      */
     protected function getWsdlUri(): string
     {
-        return $this->getDynamicWsdl();
+        return route('avantern.shipment.wsdl');
     }
 
     protected function getEndpoint(): string
